14b08be9f3df427cd6853efd0e4e3f5bcc84d6b6
Anwesh43 <anweshthecool0@gmail.com>
2017-06-04 04:09:41 +0530

    Adding and drawing CircularProgress

--- toggle-filter-card.js
+++ toggle-filter-card.js
@@ -16,11 +16,17 @@ class ToggleFilterCard extends HTMLElement {
         const context = canvas.getContext('2d')
         context.drawImage(image,0,0)
         this.colorFilter.draw(context,this.color)
+        context.save()
+        context.fillStyle = 'black'
+        context.globalAlpha = 0.55
+        context.restore()
+        this.circularProgress.draw(context,this.color)
     }
     connectedCallback() {
         const image = new Image()
         image.src = this.src
         image.onload = () => {
+            this.circularProgress = new CircularProgress(image.width,image.height)
             this.colorFilter.setMaxH(2*image.height/3)
             this.render(image)
         }
@@ -32,9 +38,11 @@ class ColorFilter {
         this.dir = 0
     }
     draw(context,color,w) {
+        context.save()
         context.fillStyle = color
         context.globalAlpha = 0.44
         context.fillRect(0,0,w,this.hx)
+        context.restore()
     }
     setMaxH(maxH) {
         this.maxH = maxH
@@ -54,6 +62,45 @@ class ColorFilter {
         if(this.hx < 0) {
             this.dir = 0
             this.hx = 0
+        }
+    }
+}
+class CircularProgress {
+    constructor(w,h) {
+        this.ea = 0
+        this.x = w/2
+        this.y = 5*h/6
+        this.dir = 0
+        this.r = h/10
+    }
+    draw(context,color) {
+        context.fillStyle = color
+        context.save()
+        context.translate(this.x,this.y)
+        context.beginPath()
+        context.moveTo(0,0)
+        for(var i=0;i<=this.ea;i++) {
+            const x = r*(Math.cos(i*Math.PI/180)) , y = r*(Math.sin(i*Math.PI/180))
+            context.lineTo(x,y)
+        }
+        context.fill()
+        context.restore()
+    }
+    setDir(dir) {
+        this.dir = dir
+    }
+    stopped() {
+        return this.dir == 0
+    }
+    update() {
+        this.ea += (72*this.dir)
+        if(this.ea > 360) {
+            this.dir = 0
+            this.ea = 360
+        }
+        if(this.ea < 0) {
+            this.dir = 0
+            this.ea = 0
         }
     }
 }
