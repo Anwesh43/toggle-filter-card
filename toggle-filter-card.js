d4c381a904e81f90d4a51c016402b040eabf609a
Anwesh43 <anweshthecool0@gmail.com>
2017-06-04 04:00:21 +0530

    Adding ColorFilter

--- toggle-filter-card.js
+++ toggle-filter-card.js
@@ -6,6 +6,7 @@ class ToggleFilterCard extends HTMLElement {
         shadow.appendChild(this.img)
         this.src = this.getAttribute('src')
         this.color = this.getAttribute('color')
+        this.colorFilter = new ColorFilter()
     }
     render(image) {
         const w = image.width , h = image.height
@@ -14,13 +15,45 @@ class ToggleFilterCard extends HTMLElement {
         canvas.height = h
         const context = canvas.getContext('2d')
         context.drawImage(image,0,0)
+        this.colorFilter.draw(context,this.color)
     }
     connectedCallback() {
         const image = new Image()
         image.src = this.src
         image.onload = () => {
+            this.colorFilter.setMaxH(2*image.height/3)
             this.render(image)
         }
-
+    }
+}
+class ColorFilter {
+    constructor() {
+        this.hx = 0
+        this.dir = 0
+    }
+    draw(context,color,w) {
+        context.fillStyle = color
+        context.globalAlpha = 0.44
+        context.fillRect(0,0,w,this.hx)
+    }
+    setMaxH(maxH) {
+        this.maxH = maxH
+    }
+    setDir(dir) {
+        this.dir = dir
+    }
+    stopped() {
+        return this.dir == 0
+    }
+    update() {
+        this.hx += ((this.maxH)/5) * this.dir
+        if(this.hx > this.maxH) {
+            this.dir = 0
+            this.hx = this.maxH
+        }
+        if(this.hx < 0) {
+            this.dir = 0
+            this.hx = 0
+        }
     }
 }
